3d9e972ffae58885b3c156d2a8ff204094b299ba
MiniEnemy <home.minj@gmail.com>
2024-10-30 16:17:25 +0545

    creating database

---
+++ My bookStore/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My_bookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ My bookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using My_bookStore.Models;
+
+namespace My_bookStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+
+    }
+   
+
+}
