15caa5da0130984cd533c8be4510a91c146c8fa5
MiniEnemy <home.minj@gmail.com>
2024-11-03 03:24:58 +0545

    added the code where u can add genre in category tables and did server sided validation

--- My bookStore/Controllers/CategoryController.cs
+++ My bookStore/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using My_bookStore.Data;
 using My_bookStore.Models;
+using System.Reflection.Metadata.Ecma335;
 
 namespace My_bookStore.Controllers
 {
@@ -9,7 +10,7 @@ namespace My_bookStore.Controllers
         private readonly ApplicationDbContext _db;
         public CategoryController(ApplicationDbContext db)
         {
-            _db = db;   
+            _db = db;
         }
         public IActionResult Index() // Displays all the categories 
         {
@@ -19,7 +20,34 @@ namespace My_bookStore.Controllers
 
         public IActionResult Create() //creates categories for genre and display the order
         {
-             return View();
+            return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.Displayorder.ToString()) // It  is checking is the Name and displayorder are same or not
+            {
+                ModelState.AddModelError("Name", "The Category Name and Display order can't be same");
+            }
+
+            if (obj.Name.ToLower() == "test" || obj.Name.ToLower() == "practice")
+            {
+                ModelState.AddModelError("", "You can't add Test or practice on Names");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
     }
+
+
 }
--- My bookStore/Models/Category.cs
+++ My bookStore/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace My_bookStore.Models
 {
@@ -7,7 +8,11 @@ namespace My_bookStore.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }
-       public int Displayorder { get; set; }
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }  //  It's like to display another name (given on displayname() ) instead of the real name
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="The number should be between 1 to 100 only ")]
+        public int Displayorder { get; set; }
     }
 }
